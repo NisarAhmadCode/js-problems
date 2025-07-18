78f75c6b24e50640960f763b057bfd610597873c
Nisar Ahmad <na4609594@gmail.com>
2025-07-18 22:05:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Greeting Msg
+let name = "Nisar"
+console.log("Welcome ," + name + "!")
+// Area of Rectangle
+let length = 10;
+let width = 5;
+console.log("Area of Rectangle is :" + length * width)
+// Simple percentage calculator
+let totalMarks = 800;
+let obtMarks = 576;
+let percentage = (obtMarks / totalMarks * 100);
+console.log("Percentage:" + percentage + "%")
+// Swap Numbers 
+let a = 10;
+let b = 5;
+let temp = a;
+a = b;
+b = temp;
+console.log("a=" + a);
+console.log("b=" + b);
+// Age Calculator
+let birthYear=2004;
+let currYear=2025;
+let age=(currYear-birthYear);
+console.log("YOur Age is:"+age)
